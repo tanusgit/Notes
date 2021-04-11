a80b019c3cd03eea92ebc17eaa07606d45f06424
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-04-11 12:00:21 -0700

    Create arrayFuntions.js

---
+++ arrayFuntions.js
@@ -0,0 +1,8 @@
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.shift(0);
+console.log(groceryList);
+groceryList.unshift('popcorn');
+console.log(groceryList);
+/*
+.join(), .slice(), .splice(), .shift(), .unshift(), and .concat()
+*/
