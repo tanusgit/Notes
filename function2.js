5671e60b42fd70572ea31a60a55ca28bf27e29bb
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-04-11 11:11:40 -0700

    Create function2.js

---
+++ function2.js
@@ -0,0 +1,10 @@
+//global variables
+const satellite = 'The Moon';
+let galaxy = 'The Milky Way';
+var stars = 'North Star';
+ 
+var callMyNightSky = () => {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+};
+ 
+console.log(callMyNightSky ());
