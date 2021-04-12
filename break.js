10d753baef14a25738a593d883d3ee96078bb0c4
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-04-11 21:25:27 -0700

    Create break.js

---
+++ break.js
@@ -0,0 +1,13 @@
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray .length; i++) {
+  if(i === 3){
+    break;
+  }
+    console.log(rapperArray[i]);
+}
+console.log("And if you don't know, now you know.");
+ 
+
+
