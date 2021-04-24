b4b0dad1063543986872d3bd9d250ffb55e71bc8
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-04-24 14:12:09 -0700

    Create arrayCallback.js

---
+++ arrayCallback.js
@@ -0,0 +1,13 @@
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+//animal is the each animal in the array
+const secretMessage = animals.map(animal => {
+  
+  return animal.charAt(0);
+});
+console.log(secretMessage.join(''));
+
+//const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
