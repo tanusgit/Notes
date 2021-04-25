11c83393e6f358f11348060e28eb8c3902e4c994
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-04-25 09:45:58 -0700

    Update Iterator.js

--- Iterator.js
+++ Iterator.js
@@ -5,3 +5,9 @@ console.log(words.some(word =>
 {
 return word.length < 6;
 }));
+
+const interestingWords = words.filter((word) => {return word.length > 5});
+
+console.log(interestingWords.every((word) => {
+  return word.length > 5;
+} ));
