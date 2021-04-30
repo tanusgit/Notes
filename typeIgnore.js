0ecb8726411baedac2ff71f642a90afa03d56b9a
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-04-30 14:10:06 -0700

    Create typeIgnore.js

---
+++ typeIgnore.js
@@ -0,0 +1,11 @@
+//type is ignored and string and numeric got added
+const robot = {
+  _energyLevel: 100,
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+
+robot._energyLevel = 'high';
+robot.recharge();
