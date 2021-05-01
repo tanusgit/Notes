efd3e6ab9fd0ae67c972c404075bde2b4e8acba9
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-05-01 08:59:42 -0700

    Create factory.js

---
+++ factory.js
@@ -0,0 +1,11 @@
+const robotFactory = (model, mobile) => {
+return {
+  model : model,
+  mobile: mobile,
+  beep(){
+    console.log('Beep Boop')
+  }
+}
+};
+const tinCan = robotFactory('P-500', true);
+tinCan.beep();
