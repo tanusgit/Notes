04d7fe8cebc9aaec80cdf82c3c78ea1b296e9c8f
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-05-07 11:18:07 -0700

    Update classVSobject.js

--- classVSobject.js
+++ classVSobject.js
@@ -16,6 +16,18 @@ class Dog {
     this._behavior ++;
   }
 }
+
+//making an instance of dog
+onst halley = new Dog('Halley');
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+halley.incrementBehavior(); // Add one to behavior
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+
+
+
+
 //object halley
 let halley = {
   _name: 'Halley',
