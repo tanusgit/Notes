dc83adbf2ff77f9ad27599e7bcddf346abda8cb7
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-06-13 09:27:16 -0700

    Create promisewithCatch.js

---
+++ promisewithCatch.js
@@ -0,0 +1,38 @@
+//app.js
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectReason) => {
+  console.log(rejectReason);
+};
+
+// Write your code below:
+
+checkInventory(order).then(handleSuccess).catch(handleFailure);
+
+//library.js
+const inventory = {
+  sunglasses: 0,
+  pants: 1088,
+  bags: 1344
+};
+
+const checkInventory = (order) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let inStock = order.every(item => inventory[item[0]] >= item[1]);
+            if (inStock) {
+                resolve(`Thank you. Your order was successful.`);
+            } else {
+                reject(`We're sorry. Your order could not be completed because some items are sold out.`);
+            }
+        }, 1000);
+    });
+};
+
+module.exports = {checkInventory};
