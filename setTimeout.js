8307478c7bcc04315816ec34ac8816b2c4bcd03d
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-06-13 08:55:44 -0700

    Create setTimeout.js

---
+++ setTimeout.js
@@ -0,0 +1,9 @@
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+
+const usingSTO = () =>{
+console.log("this is a string");
+}
+
+setTimeout(usingSTO, 3000);
