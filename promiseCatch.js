6279b88a3b4065f66733119a2a5eb1b41452cf6b
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-06-13 12:03:03 -0700

    Create promiseCatch.js

---
+++ promiseCatch.js
@@ -0,0 +1,42 @@
+//app.js
+const {checkAvailability} = require('./library.js');
+
+const onFulfill = (itemsArray) => {
+  console.log(`Items checked: ${itemsArray}`);
+  console.log(`Every item was available from the distributor. Placing order now.`);
+};
+
+const onReject = (rejectionReason) => {
+	console.log(rejectionReason);
+};
+
+// Write your code below:
+const checkSunglasses = checkAvailability('sunglasses','Favorite Supply Co.');
+const checkPants = checkAvailability('pants','Favorite Supply Co.');
+const checkBags = checkAvailability('bags','Favorite Supply Co.');
+const promiseArray 
+=[checkSunglasses, checkPants, checkBags];       
+Promise.all(promiseArray).then(onFulfill).catch(onReject);
+//library.js
+const checkAvailability = (itemName, distributorName) => {
+    console.log(`Checking availability of ${itemName} at ${distributorName}...`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (restockSuccess()) {
+                console.log(`${itemName} are in stock at ${distributorName}`)
+                resolve(itemName);
+            } else {
+                reject(`Error: ${itemName} is unavailable from ${distributorName} at this time.`);
+            }
+        }, 1000);
+    });
+};
+
+module.exports = { checkAvailability };
+
+
+// This is a function that returns true 80% of the time
+// We're using it to simulate a request to the distributor being successful this often
+function restockSuccess() {
+    return (Math.random() > .2);
+}
