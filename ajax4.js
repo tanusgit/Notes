e7b07584e5bfeab61946962260f7e8c34dbdac1a
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-06-20 11:55:05 -0700

    Create ajax4.js

---
+++ ajax4.js
@@ -0,0 +1,12 @@
+//main.js
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint';
+const data = JSON.stringify({id: '200'});
+xhr.responseType = 'json';
+xhr.onreadystatechange = () =>{
+  if(xhr.readyState ===XMLHttpRequest.DONE){
+    return xhr.response = data;
+  }
+}
+xhr.open('POST', url);
+xhr.send(data);
