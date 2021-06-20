7d8643421fcf1efb3dcbe565153ed95c66b8bcf9
Tanu <24976766+tanusgit@users.noreply.github.com>
2021-06-19 21:07:34 -0700

    Create asyncawait5.js

---
+++ asyncawait5.js
@@ -0,0 +1,49 @@
+//app.js
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js');
+
+// Write your code below:
+async function serveDinner(){
+  let vegetablePromise = steamBroccoli();
+  let starchPromise =  cookRice();
+  let proteinPromise =  bakeChicken();
+  let sidePromise = cookBeans();
+  console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`);
+}
+serveDinner();
+
+
+
+//library.js
+let cookBeans = () => {
+  return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('beans');
+   }, 1000);
+ });
+}
+
+let steamBroccoli = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('broccoli');
+   }, 1000);
+ });
+}
+
+let cookRice = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('rice');
+   }, 1000);
+ });
+}
+
+let bakeChicken = () => {
+ return new Promise ((resolve, reject) => {
+   setTimeout(()=>{
+     resolve('chicken');
+   }, 1000);
+ });
+}
+
+module.exports = {cookBeans, steamBroccoli, cookRice, bakeChicken};
